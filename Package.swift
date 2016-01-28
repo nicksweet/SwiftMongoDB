f25d6516aa5763896adedbd08c7bdd2df6dcb8a6
Nick Sweet <nick@silo.io>
2016-01-28 12:57:34 -0800

    remove dependencies from Package.swift

--- Package.swift
+++ Package.swift
@@ -2,8 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "CFServer",
-    dependencies: [
-        .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/PureSwift/MongoDB.git", majorVersion: 0),
-    ]
+    dependencies: []
 )
